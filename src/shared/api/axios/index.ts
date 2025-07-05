ce2a9c463e0441d175ea8ec5cf00925136aa836a
Atash03 <hugokarp12@gmail.com>
2025-07-05 23:37:58 +0500

    fix(feature): scroll pagintion fix

--- src/shared/api/axios/index.ts
+++ src/shared/api/axios/index.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.BASE_URL;
-
 export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 10000,
 });
