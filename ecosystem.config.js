759c06db16b9530cc02028446e8a6badf92db715
Atash03 <hugokarp12@gmail.com>
2025-08-11 16:29:44 +0500

    feat(setup): setup pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "orient",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3001",
+      cwd: "/home/tpsuser/orient-web",
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
