07cb6519e1208ef8144a48e0b193a713ee029479
Atash03 <hugokarp12@gmail.com>
2025-08-24 20:58:36 +0500

    fix: remove google font &  add local font

--- src/shared/lib/theme/fonts.ts
+++ src/shared/lib/theme/fonts.ts
@@ -1,7 +1,28 @@
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
-export const inter = Inter({
-  subsets: ['latin'],
+export const inter = localFont({
+  src: [
+    {
+      path: '../../../../public/inter/Inter_18pt-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../../../public/inter/Inter_18pt-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../../../public/inter/Inter_18pt-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../../../public/inter/Inter_18pt-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
   variable: '--font-inter',
   display: 'swap',
 });
